91f00243e5ee74f4b8c682bda177b94fff67a973
yashvinthan <yashvinthanmoorthy@gmail.com>
2026-09-25 00:04:32 +0530

    refactor: consolidate into single-folder repository with self-contained 3D workstation, OS, and competition arena

--- portfolio-inner-site/src/components/general/index.ts
+++
@@ -1,6 +0,0 @@
-import Link from './Link';
-import Icon from './Icon';
-import MusicPlayer from './MusicPlayer';
-import Colors from '../../constants/colors';
-
-export { Icon, Colors, MusicPlayer, Link };
